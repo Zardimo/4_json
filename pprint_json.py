cda6aa55f5caa8cd9b73c2f80941d88061efb4e9
Zardimo <vasyasapeggin@rambler.ru>
2019-02-11 09:20:58 +0500

    Исправления внесены

--- pprint_json.py
+++ pprint_json.py
@@ -7,21 +7,21 @@ import sys
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    with open(filepath, 'r', encoding='utf-8') as prettystyle:
-        return json.load(prettystyle)
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
 
-def pretty_print_json(base_for_change):
-    return json.dumps(base_for_change, indent=4, ensure_ascii=False)
+def pretty_print_json(python_object):
+    return json.dumps(python_object, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
     if not len(sys.argv) > 1:
         exit('Укажите путь к файлу')
     try:
-        base_for_change = load_data(argv[1])
+        python_object = load_data(argv[1])
     except json.decoder.JSONDecodeError:
         exit('Данные в файле должны быть формата json')
-    if not base_for_change:
+    if not python_object:
         exit('Укажите верный путь к файлу')
-    print(pretty_print_json(base_for_change))
+    print(pretty_print_json(python_object))
